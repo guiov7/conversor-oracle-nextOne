0749e0abb59a2cef2bb3a61025f886e849d35e85
guiov7 <guio.developer@gmail.com>
2023-08-14 21:24:01 -0300

    adicionando funções de conversão de Moedas e separando serviços

--- src/br/com/model/ConversorMoedas/Funcao.java
+++ src/br/com/model/ConversorMoedas/Funcao.java
@@ -1,12 +1,57 @@
 package src.br.com.model.ConversorMoedas;
-import javax.swing.*;
+
+import src.br.com.model.ConversorMoedas.ConverteMoedas;
+import src.br.com.model.ConversorMoedas.ConverteParaReais;
+
+import javax.swing.JOptionPane;
+import java.util.Objects;
 
 public class Funcao {
-    
-    ConversorMoedas moedas = new ConversorMoedas();
+
+    ConverteMoedas moedas = new ConverteMoedas();
+    ConverteParaReais reais = new ConverteParaReais();
 
     public void converterMoedas(double valorRecebido) {
 
-        
+        String opcao = (JOptionPane.showInputDialog(null, "Escolha a moeda a qual deseja converter", "Moedas",
+                JOptionPane.PLAIN_MESSAGE, null,
+                new Object[] { "De Reais para Dólares", "De Reais para Euros", "De Reais para Libras Esterlinas",
+                        "De Reais para Pesos Argentinos", "De Reais para Pesos Chilenos",
+                        "De Dólares para Reais", "De Euros para Reais", "De Libras Esterlinas para Reais",
+                        "De Pesos Argentinos para Reais", "De Pesos Chilenos para Reais" },
+                "Escolha: ")).toString();
+
+        switch (opcao) {
+            case "De Reais para Dólares":
+                moedas.converterReaisParaDolares(valorRecebido);
+                break;
+            case "De Reais para Euros":
+                moedas.converterReaisParaEuro(valorRecebido);
+                break;
+            case "De Reais para Libras Esterlinas":
+                moedas.converterReaisParaLibras(valorRecebido);
+                break;
+            case "De Reais para Pesos Argentinos":
+                moedas.converterReaisParaPesosArg(valorRecebido);
+                break;
+            case "De Reais para Pesos Chilenos":
+                moedas.converterReaisParaPesosChil(valorRecebido);
+                break;
+            case "De Dólares para Reais":
+                reais.converterDolaresParaReais(valorRecebido);
+                break;
+            case "De Euros para Reais":
+                reais.converterEurosParaReais(valorRecebido);
+                break;
+            case "De Libras Esterlinas para Reais":
+                reais.converterLibrasParaReais(valorRecebido);
+                break;
+            case "De Pesos Argentinos para Reais":
+                reais.converterPesosArgParaReal(valorRecebido);
+                break;
+            case "De Pesos Chilenos para Reais":
+                reais.converterPesosChilParaReal(valorRecebido);
+                break;
+        }
     }
 }
--- src/br/com/model/Main/Main.java
+++ src/br/com/model/Main/Main.java
@@ -1,6 +1,7 @@
 package src.br.com.model.Main;
 
-import src.br.com.model.ConversorMoedas.ConversorMoedas;
+import src.br.com.model.ConversorMoedas.ConverteMoedas;
+import src.br.com.model.ConversorMoedas.Funcao;
 import src.br.com.model.ConversorTemperatura.ConversorTemperatura;
 
 import javax.swing.*;
@@ -9,7 +10,42 @@ public class Main {
 
     public static void main(String[] args) {
         
-        
+        Funcao moedas = new Funcao();
 
+        while (true) {
+
+            String opcao = JOptionPane.showInputDialog(null, "Escolha uma opção ", "Menu", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Conversor de Moeda", "Conversor de Temperatura"}, "Escolha").toString();
+
+            switch (opcao) {
+                case "Conversor de Moeda":
+                    String input = JOptionPane.showInputDialog("Insira um valor: ");
+                    if (checar(input)) {
+                        double valorRecebido = Double.parseDouble(input);
+                        moedas.converterMoedas(valorRecebido);
+
+                        int resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+                        if (JOptionPane.OK_OPTION == resposta) {
+                            System.out.println("Escolha opção Afirmativa");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Valor inválido");
+                    }
+                break;
+
+            }
+   
+        }
+
+    }
+    public static boolean checar(String input) {
+        try {
+            double x = Double.parseDouble(input);
+            if (x >= 0 || x < 0) ;
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
